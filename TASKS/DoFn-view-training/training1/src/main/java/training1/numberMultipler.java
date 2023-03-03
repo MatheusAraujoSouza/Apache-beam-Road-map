cce2c676ba3f648d56ac0803693acb5ac7ea2b37
Matheus Araujo Souza <mhatheussss@gmail.com>
2023-03-02 22:54:14 -0300

    new methods added

--- TASKS/DoFn-view-training/training1/src/main/java/training1/numberMultipler.java
+++ TASKS/DoFn-view-training/training1/src/main/java/training1/numberMultipler.java
@@ -3,61 +3,80 @@ package training1;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.testing.TestStream;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.transforms.Sum;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
-import org.apache.beam.sdk.values.TimestampedValue;
 import org.apache.beam.sdk.values.TypeDescriptors;
-import org.joda.time.Duration;
-import org.joda.time.Instant;
 
 public class numberMultipler {
 
   public static void main(String[] args) {
     Pipeline pipeline = Pipeline.create();
 
-    // Create mock input data with timestamps
-    TestStream<String> input = TestStream.create(StringUtf8Coder.of())
-        .addElements(TimestampedValue.of("1", Instant.now()))
-        .advanceWatermarkToInfinity();
+    // Create mock input data
+    PCollection<Integer> numbers = pipeline.apply(Create.of(1, 2, 3, 4, 5));
 
-    PCollection<Integer> numbers = pipeline.apply(input)
-        .apply(MapElements.into(TypeDescriptors.integers()).via(Integer::valueOf));
+    PCollection<Integer> multipliedNumbers = numbers.apply(ParDo.of(new DoFn<Integer, Integer>() {
+      /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
-    // Apply FixedWindows of 1 minute to the input data
-    PCollection<Integer> windowedNumbers = numbers.apply(Window.into(FixedWindows.of(Duration.standardMinutes(1))));
-
-    PCollection<Double> multipliedAndSummedNumbers = windowedNumbers.apply(ParDo.of(new DoFn<Integer, Double>() {
-      @ProcessElement
+	@ProcessElement
       public void processElement(ProcessContext c) {
         Integer input = c.element();
-        Double output = input * 30.0 + 12.0;
+        Integer output = input * 30;
         c.output(output);
       }
-    })).apply(Sum.doublesGlobally());
+    }));
+    
+       
+    PCollection<Double> divideNumber = multipliedNumbers.apply(ParDo.of(new DoFn<Integer,Double >(){
+    	@ProcessElement
+    	public void processElement(ProcessContext c) {
+            Integer input = c.element();
+    		 double output = input.doubleValue() / 5.0;
+    	        c.output(output);
+    	}
+    }));
+    
+    PCollection<Double> SumNumberPerElement = multipliedNumbers.apply(ParDo.of(new DoFn<Integer,Double >(){
+    	@ProcessElement
+    	public void processElement(ProcessContext c) {
+            Integer input = c.element();
+    		 double output = input.doubleValue() + 12.0;
+    	        c.output(output);
+    	}
+    }));
+    
+    
+    PCollection<Double> sum = divideNumber.apply(Sum.doublesGlobally());
+    
+    PCollection<Double> convertedDivideNumber = divideNumber.apply(MapElements.into(TypeDescriptors.doubles()).via(Double::valueOf));
+    
+    PCollectionList<Double> collections = PCollectionList.of(sum).and(convertedDivideNumber);
+    PCollection<Double> combined = collections.apply(Flatten.<Double>pCollections());
 
-    PCollection<Float> dividedNumbers = multipliedAndSummedNumbers.apply(MapElements.into(TypeDescriptors.floats()).via(new SerializableFunction<Double, Float>() {
-      @Override
-      public Float apply(Double input) {
-        return input.floatValue() / 5.0f;
-      }
+    
+
+    //To print the output value 
+    
+    combined.apply(MapElements.into(TypeDescriptors.strings()).via(Object::toString))
+    .apply("PrintNumbers",ParDo.of(new DoFn<String,Void>(){
+    	
+    	@ProcessElement
+    	public void processElement(ProcessContext c) {
+        	System.out.println(c.element());
+
+    	}
     }));
 
-    // To print the output value
-    dividedNumbers.apply(MapElements.into(TypeDescriptors.strings()).via(Object::toString))
-        .apply("PrintNumbers",ParDo.of(new DoFn<String,Void>(){
-          @ProcessElement
-          public void processElement(ProcessContext c) {
-            System.out.println(c.element());
-          }
-        }));
+
 
     pipeline.run();
   }
